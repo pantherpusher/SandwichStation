22c7afa08bddaf85449c3a57d846c29d77c762cb
ShibbyBot <shibby@inucast.com>
2025-05-16 18:50:57 +0000

    chore: Automatically update REUSE headers

--- Content.Client/Administration/Systems/AdminSystem.Overlay.cs
+++ Content.Client/Administration/Systems/AdminSystem.Overlay.cs
@@ -44,6 +44,8 @@
 // SPDX-FileCopyrightText: 2024 stellar-novas <stellar_novas@riseup.net>
 // SPDX-FileCopyrightText: 2024 themias <89101928+themias@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Errant <35878406+Errant-4@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
